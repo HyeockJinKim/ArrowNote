616f80852cd8a408ea951ea65f3147799e7a6f37
KimHyeockJin <gurwls9628@naver.com>
2018-04-08 23:04:10 +0900

    Initial design
    modify fxml to make the initial UI.
    modify Controller to adjust initial UI settings.
    
    Closes https://github.com/HyeockJinKim/ArrowNote/issues/2

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,8 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("arrowNote.fxml"));
+        primaryStage.setTitle("Arrow Note");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
